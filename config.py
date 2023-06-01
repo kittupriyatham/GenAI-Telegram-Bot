263789a21053d0cec6d38a3df205c9bf8143ca0b
Krishna Priyatham Potluri <kittupriyatham@gmail.com>
2023-06-01 21:18:26 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-from typing import Final
-
-TOKEN: Final = "5692270027:AAF6TeCOMcyATcdjQqapxBJ9xxBzYDbfFQk"
-
-ORG: Final = "org-ivUo0RjnIgM998diK8T4pQLr"
-KEY: Final = "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"
